05136348c6c5999f550aca82f7959f40a2ee858b
Arshad <mohammedarshad183@gmail.com>
2022-06-12 13:11:33 +0530

    Added : Retrived the data from database and displayed

--- EmployeePayRoll/EmployeePayRoll/EmployeeRepo.cs
+++ EmployeePayRoll/EmployeePayRoll/EmployeeRepo.cs
@@ -24,7 +24,38 @@ namespace EmployeePayRoll
                 SqlDataReader dr = cmd.ExecuteReader();
                 if (dr.HasRows)
                 {
-                    Console.WriteLine("Connection Established Successfully !!");
+                    while (dr.Read())
+                    {
+                        employeeModel.EmployeeId = dr.GetInt32(0);
+                        employeeModel.EmployeeName = dr.GetString(1);
+                        employeeModel.Phone_Number = Convert.ToInt64(dr[2] is DBNull ? 0 : dr[2]);
+                        employeeModel.Address = Convert.ToString(dr[3]) is DBNull ? string.Empty : dr[3].ToString();
+                        employeeModel.Department = dr.GetString(4);
+                        employeeModel.Gender = Convert.ToString(dr[5]) is DBNull ? string.Empty : dr[5].ToString();
+                        employeeModel.Basic_Pay = dr.GetDouble(6);
+                        employeeModel.Deductions = dr.GetDouble(7);
+                        employeeModel.Taxable_Pay = dr.GetDouble(8);
+                        employeeModel.Tax = dr.GetDouble(9);
+                        employeeModel.Net_Pay = dr.GetDouble(10);
+                        employeeModel.StartDate = dr.GetDateTime(11);
+                        employeeModel.City = dr.GetString(12);
+                        employeeModel.Country = dr.GetString(13);
+
+                        Console.WriteLine("EmoloyeeId      :  " + employeeModel.EmployeeId);
+                        Console.WriteLine("EmoloyeeName    :  " + employeeModel.EmployeeName);
+                        Console.WriteLine("Phone_Number    :  " + employeeModel.Phone_Number);
+                        Console.WriteLine("Address         :  " + employeeModel.Address);
+                        Console.WriteLine("Department      :  " + employeeModel.Department);
+                        Console.WriteLine("Gender          :  " + employeeModel.Gender);
+                        Console.WriteLine("Basic_Pay       :  " + employeeModel.Basic_Pay);
+                        Console.WriteLine("Deductions      :  " + employeeModel.Deductions);
+                        Console.WriteLine("Taxable_Pay     :  " + employeeModel.Taxable_Pay);
+                        Console.WriteLine("Tax             :  " + employeeModel.Tax);
+                        Console.WriteLine("Net_Pay         :  " + employeeModel.Net_Pay);
+                        Console.WriteLine("StartDate       :  " + employeeModel.StartDate);
+                        Console.WriteLine("City            :  " + employeeModel.City);
+                        Console.WriteLine("Country         :  " + employeeModel.Country + "\n\n");
+                    }
                 }
             }
         }
